7b0d4fcaf1b51f7620642aea3f15902544c62e5c
mwawrzen <w.w@op.pl>
2022-01-01 17:58:25 +0100

    Add endpoint /data

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const me = {
+  imie: 'Michał',
+  nazwisko: 'Wawrzeń',
+  klasa: '3i2',
+  grupa: 'a'
+};
+
 app.get('/', (req, res) => {
-    res.send("<h1>my app on heroku!</h1>")
+    res.send('<h1>my app on heroku!</h1>');
+});
+
+app.get('/data', (req, res) => {
+  res.send(me);
 });
 
 app.listen(PORT, () => {
